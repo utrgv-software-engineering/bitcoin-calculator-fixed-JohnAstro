c7c08c483a2de6287778e9f6c7dde58690fcaf01
JohnAstro <jonathan.gonzalez12@utrgv.edu>
2022-04-11 03:14:58 -0500

    bitcoin api fucntion returns future double

--- lib/utils/bitcoin_api.dart
+++ lib/utils/bitcoin_api.dart
@@ -3,14 +3,17 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class BitcoinAPI {
-  static Future<CurrentBTCInUSD> fetchCurrentBTCInUSD(
-      http.Client client) async {
+  static Future<double> fetchCurrentBTCInUSD(http.Client client) async {
     var url =
         Uri.parse('https://api.coindesk.com/v1/bpi/currentprice/usd.json');
     final response = await client.get(url);
+    Map<String, dynamic> json = jsonDecode(response.body);
+
+    CurrentBTCInUSD currentRate =
+        CurrentBTCInUSD(rateFloat: json['bpi']['USD']['rate_float']);
 
     if (response.statusCode == 200) {
-      return CurrentBTCInUSD.fromJson(jsonDecode(response.body));
+      return currentRate.rateFloat;
     } else {
       throw Exception('Failed to load current price of BTC in USD');
     }
--- lib/utils/current_bitcoin_rate.dart
+++ lib/utils/current_bitcoin_rate.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 class CurrentBTCInUSD {
   final double rateFloat;
 
